0d18777fa0ce3073a8eef2abc7e6a0e1f85f290b
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-01-15 14:30:57 +0000

    add views files

--- src/models/solar_system.js
+++ src/models/solar_system.js
@@ -2,4 +2,9 @@ const SolarSystem = function(planets) {
   this.planets = planets;
 };
 
+SolarSystem.prototype.bindEvents = function () {
+  PubSub.publish('SolarSystem:all-planets-ready', this.planets);
+
+};
+
 module.exports = SolarSystem;
---
+++ src/views/planet_info_view.js
@@ -0,0 +1,8 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const PlanetInfoView = function () {
+
+
+};
+
+module.exports = PlanetInfoView;
